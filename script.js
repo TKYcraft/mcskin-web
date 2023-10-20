788d7456a0e6332c065440fa91653f09d50ca1fc
Alicey <sirius@alicey.dev>
2023-10-20 23:23:27 +0900

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+function getSkin() {
+    const minecraftId = document.getElementById("minecraftId").value;
+    const skinImage = document.getElementById("skinImage");
+
+    if (minecraftId) {
+        const apiUrl = `https://chisato.alicey.dev/api/v1/texture/face/${minecraftId}.png`;
+        
+        // APIから画像を取得
+        fetch(apiUrl)
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                }
+                throw new Error('Network response was not ok.');
+            })
+            .then(blob => {
+                // 画像を表示
+                const imgUrl = URL.createObjectURL(blob);
+                skinImage.src = imgUrl;
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+                skinImage.src = ''; // エラー時に画像をクリア
+            });
+    } else {
+        alert("Minecraft IDを入力してください！");
+    }
+}
