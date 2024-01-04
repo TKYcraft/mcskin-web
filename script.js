f525a11036c9034428314b2f736131c15e6e2aac
Alicey <54524362+Alicey0719@users.noreply.github.com>
2024-01-04 18:09:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function getSkin() {
     const minecraftId = document.getElementById("minecraftId").value;
     const skinImage = document.getElementById("skinImage");
     const resultMessage = document.getElementById("resultMessage");
-
+    
     if (minecraftId) {
         const apiUrl = `https://chisato.alicey.dev/api/v1/texture/face/${minecraftId}.png`;
 
         // APIから画像を取得
         fetch(apiUrl)
-            .then((response) => {
+            .then(response => {
                 if (response.ok) {
                     return response.blob();
                 }
-                throw new Error("Network response was not ok.");
+                throw new Error('Network response was not ok.');
             })
             .then((blob) => {
                 // 画像を表示
