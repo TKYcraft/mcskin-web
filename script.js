36127a404ca5d80fc25e82e2c5c5a1fb9c48d806
Alicey <sirius@alicey.dev>
2024-01-04 18:05:11 +0900

    Update search on keybord

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ function getSkin() {
 
     if (minecraftId) {
         const apiUrl = `https://chisato.alicey.dev/api/v1/texture/face/${minecraftId}.png`;
-        
+
         // APIから画像を取得
         fetch(apiUrl)
-            .then(response => {
+            .then((response) => {
                 if (response.ok) {
                     return response.blob();
                 }
-                throw new Error('Network response was not ok.');
+                throw new Error("Network response was not ok.");
             })
-            .then(blob => {
+            .then((blob) => {
                 // 画像を表示
                 const imgUrl = URL.createObjectURL(blob);
                 skinImage.src = imgUrl;
             })
-            .catch(error => {
-                console.error('There has been a problem with your fetch operation:', error);
-                skinImage.src = 'https://chisato.alicey.dev/api/v1/texture/face/shigure_alicey.png';
+            .catch((error) => {
+                console.error(
+                    "There has been a problem with your fetch operation:",
+                    error
+                );
+                skinImage.src =
+                    "https://chisato.alicey.dev/api/v1/texture/face/shigure_alicey.png";
             });
     } else {
         alert("Minecraft IDを入力してください！");
+    }
+}
+
+function IDTextbox_keydown(e) {
+    if (e.key === "Enter") {
+        getSkin();
     }
 }
